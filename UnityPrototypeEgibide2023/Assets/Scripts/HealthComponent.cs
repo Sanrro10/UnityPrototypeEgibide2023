9fc5f8cdb36827fbf4f1455391419bb62371ef25
egoitzperezdearrilucea <egoitz.perezdearrilucea@ikasle.egibide.org>
2023-10-10 19:22:58 +0200

    [Egoitz] Health system
    can add/remove health

---
+++ UnityPrototypeEgibide2023/Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    private int _healthPoints;
+    
+    
+    public void Set(int points)
+    {
+        _healthPoints = points;
+    }
+
+    public int Get()
+    {
+        return _healthPoints;
+    }
+
+    public void AddHealth(int points)
+    {
+        _healthPoints = _healthPoints + points;
+    }
+    
+    public void RemoveHealth(int points)
+    {
+        _healthPoints = _healthPoints - points;
+
+        //Death animation
+        if (_healthPoints <= 0)
+        {
+            
+            //TO add call to parents animation
+            Debug.Log("Se murio ):");
+        }
+    }    
+}
