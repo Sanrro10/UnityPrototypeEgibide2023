2c1ffc971bf79ace0e5732b07851f2e6a1700d2e
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2023-10-20 18:03:50 +0200

    Animations Done

--- UnityPrototypeEgibide2023/Assets/Scripts/PlayerMovement.cs
+++ UnityPrototypeEgibide2023/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
@@ -13,20 +14,28 @@ public class PlayerMovement : MonoBehaviour
     private Boolean _onDownAttack;
     private Boolean _onDash;
     private Boolean _onDashCooldown;
+    private bool facingRight = true;
+    
+    
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    
     
     [SerializeField] private PlayerData playerData;
     
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _controls = new InputActions();
-
+        
         //Enable the actions
         _controls.Enable();
 
         //Movement
         _controls.GeneralActionMap.Movement.started += ctx => InvokeRepeating(nameof(Move), 0, 0.01f);
-        _controls.GeneralActionMap.Movement.canceled += ctx => CancelInvoke(nameof(Move));
+        _controls.GeneralActionMap.Movement.canceled += ctx => CancelMoving();
 
         //Jump
         _controls.GeneralActionMap.Jump.performed += ctx => Jump();
@@ -35,16 +44,18 @@ public class PlayerMovement : MonoBehaviour
         _controls.GeneralActionMap.Dash.performed += ctx => Dash();
     }
 
+
     void Move()
     {
+        _animator.SetBool("IsMoving", true);
         if (!_onDash)
         {
-
             Vector2 direccion = _controls.GeneralActionMap.Movement.ReadValue<Vector2>();
 
             if (direccion.Equals(Vector2.left) || direccion.Equals(Vector2.right))
             {
-                playerData.facingRight = direccion.x == 1 ? true : false;
+                facingRight = direccion.x == 1 ? true : false;
+                _spriteRenderer.flipX = !facingRight;
                 transform.position += new Vector3(playerData.movementSpeed * direccion.x, 0, 0);
             }
 
@@ -57,18 +68,28 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void CancelMoving()
+    {
+        CancelInvoke(nameof(Move));
+        _animator.SetBool("IsMoving", false);
+
+    }
+
     void Jump()
     {
         if (!_onDownAttack)
         {
             if (!_onAir)
             {
+                _animator.SetBool("OnAir", true);
+                _animator.SetTrigger("Jump");
                 GetComponent<Rigidbody2D>().velocity = Vector2.up * playerData.jumpPower;
                 _onAir = true;
                 _onDoubleJump = false;
             }
             else if (!_onDoubleJump)
             {
+                _animator.SetTrigger("Jump");
                 GetComponent<Rigidbody2D>().velocity = Vector2.up * playerData.jumpPower;
                 _onDoubleJump = true;
             }
@@ -82,6 +103,7 @@ public class PlayerMovement : MonoBehaviour
         {
             _onDashCooldown = true;
             _onDash = true;
+            _animator.SetTrigger("Dash");
             StartCoroutine(dashDuration());
             float dashValue = (playerData.movementSpeed * 100) * playerData.dashSpeed;
             dashValue *= (playerData.facingRight ? 1 : -1);
@@ -100,9 +122,10 @@ public class PlayerMovement : MonoBehaviour
     // --------------- EVENTS ----------------------
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      
+        
         if (collision.gameObject.tag == "Floor")
         {
+            _animator.SetBool("OnAir", false);
             _onAir = false;
             _onDoubleJump = false;
             _onDownAttack = false;
