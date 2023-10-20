9d615ab597ee5a238be94a5ec2b8b24ed37d6353
Raul Estrada <raul6114@gmail.com>
2023-10-20 17:19:27 +0200

    [Raul] Added 2 enemies

--- UnityPrototypeEgibide2023/Assets/Scripts/PlayerMovement.cs
+++ UnityPrototypeEgibide2023/Assets/Scripts/PlayerMovement.cs
@@ -97,7 +97,6 @@ public class PlayerMovement : MonoBehaviour
     // --------------- EVENTS ----------------------
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      
         if (collision.gameObject.tag == "Floor")
         {
             _onAir = false;
