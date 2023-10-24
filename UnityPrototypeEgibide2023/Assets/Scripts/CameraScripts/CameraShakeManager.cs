e15b1569d21f6e044fc39ef0fcc1f07513252b84
Markel <markel.fernandezdelabastida@ikasle.egibide.org>
2023-10-24 18:00:08 +0200

    [Markel, David] Camera shaking started, not finished

---
+++ UnityPrototypeEgibide2023/Assets/Scripts/CameraScripts/CameraShakeManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraShakeManager : MonoBehaviour
+{
+    // [SerializeField] private Pla
+    public static CameraShakeManager instance;
+
+    [SerializeField] private float globalShake = 1f;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void CameraShake(CinemachineImpulseSource impulseSource)
+    {
+        impulseSource.GenerateImpulseWithForce(globalShake);
+    }
+}
