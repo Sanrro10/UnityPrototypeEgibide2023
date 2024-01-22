054d3aea345e4c98f634f871de89a8e16c9f6aef
egoitzperezdearrilucea <egoitz.perezdearrilucea@ikasle.egibide.org>
2024-01-22 20:58:51 +0100

    [Egoitz] Faling rocks

--- UnityPrototypeEgibide2023/Assets/Entities/Enemies/Falling Rock/FallingRock_Script.cs
+++ UnityPrototypeEgibide2023/Assets/Entities/Enemies/Falling Rock/FallingRock_Script.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Entities;
 using UnityEngine;
 
-public class FallingRock_Script : MonoBehaviour
+public class FallingRock_Script : EntityControler
 {
+    private bool isMoving = false;
+    private Vector3 rbVelocity;
+    [SerializeField] private Rigidbody Rb;
+    [SerializeField] GameObject collider;
+ 
+    void FixedUpdate ()
+    {
+        rbVelocity = Rb.velocity;
+        if (isMoving && rbVelocity == Vector3.zero)
+        {
+            //rb has stopped moving
+            Debug.Log("la piedra cayo");
+            isMoving = false;
+            Destroy(gameObject);            
+        }
+        else if (!isMoving && rbVelocity != Vector3.zero)
+        {
+            isMoving = true;
+        }
+    }
+    
+    
     // Start is called before the first frame update
     void Start()
     {
