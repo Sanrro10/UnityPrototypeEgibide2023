b13911539163e15cce9da977222bad6421821177
egoitzperezdearrilucea <egoitz.perezdearrilucea@ikasle.egibide.org>
2024-01-23 17:50:35 +0100

    [Egoitz] Cool particles

--- UnityPrototypeEgibide2023/Assets/Entities/Enemies/Falling Rock/FallingRock_Script.cs
+++ UnityPrototypeEgibide2023/Assets/Entities/Enemies/Falling Rock/FallingRock_Script.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class FallingRock_Script : EntityControler
 {
     private bool isMoving = false;
+    private bool asExploded = false;
     private Vector3 rbVelocity;
     [SerializeField] private Rigidbody Rb;
     [SerializeField] GameObject collider;
+    [SerializeField] GameObject rock;
+    [SerializeField] GameObject explotion;
+    private GameObject spawnedExplotion;
  
     void FixedUpdate ()
     {
@@ -16,14 +20,33 @@ public class FallingRock_Script : EntityControler
         rbVelocity = Rb.velocity;
         
         //La piedra ha dejado de moverse
-        if (isMoving && rbVelocity == Vector3.zero)
+        if (isMoving && rbVelocity == Vector3.zero && !asExploded)
         {
-            //Borrar la piedra
-            Destroy(gameObject);            
+            //Instanciate explotion
+            asExploded = true;
+            spawnedExplotion = Instantiate(explotion, transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z), Quaternion.identity);
+            rock.SetActive(false);
+            
+            Invoke("DeleteExplotion", 1);
+            Invoke("DeleteRock", 1);
+
+                      
         }
         else if (!isMoving && rbVelocity != Vector3.zero)
         {
             isMoving = true;
         }
     }
+    
+    private void DeleteExplotion()
+    { 
+        Destroy(spawnedExplotion);
+    }    
+    
+    private void DeleteRock()
+    { 
+        Destroy(gameObject);
+        
+    }
+    
 }
