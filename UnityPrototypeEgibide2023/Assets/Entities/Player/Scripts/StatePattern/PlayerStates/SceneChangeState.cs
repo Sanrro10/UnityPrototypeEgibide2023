ad59107f411a64932d0199184413ec28944e40c8
Jon Maneiro <jonmaneiro1999@gmail.com>
2024-01-29 18:32:39 +0100

    Uuuuuuuuuuuuuuu ahora no hace falta quitarle cosas

--- UnityPrototypeEgibide2023/Assets/Entities/Player/Scripts/StatePattern/PlayerStates/SceneChangeState.cs
+++ UnityPrototypeEgibide2023/Assets/Entities/Player/Scripts/StatePattern/PlayerStates/SceneChangeState.cs
@@ -19,8 +19,8 @@ namespace Entities.Player.Scripts.StatePattern.PlayerStates
 
         public void Enter()
         {
-            player.GetComponent<CapsuleCollider2D>().enabled = false;
-            player.GetComponent<Rigidbody2D>().simulated= false;
+            //player.GetComponent<CapsuleCollider2D>().enabled = false;
+            //player.GetComponent<Rigidbody2D>().simulated= false;
             player.DisablePlayerControls();
             playerSpawnData = player.GetSPlayerSpawnData();
             player.animator.SetBool("IsWalk", true);
@@ -49,8 +49,8 @@ namespace Entities.Player.Scripts.StatePattern.PlayerStates
 
         public void Exit()
         {
-            player.GetComponent<CapsuleCollider2D>().enabled = true;
-            player.GetComponent<Rigidbody2D>().simulated= true;
+            //player.GetComponent<CapsuleCollider2D>().enabled = true;
+            //player.GetComponent<Rigidbody2D>().simulated= true;
             player.EnablePlayerControls();
             player.animator.SetBool("IsWalk", false);
         }
