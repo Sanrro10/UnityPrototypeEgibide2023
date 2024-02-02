57f5b3a87b42960c88e06ef58719d9eb2fe4c761
Your <you@example.com>
2024-02-02 19:28:23 +0100

    GameController

--- UnityPrototypeEgibide2023/Assets/General/Scripts/GameController.cs
+++ UnityPrototypeEgibide2023/Assets/General/Scripts/GameController.cs
@@ -4,8 +4,6 @@ using Entities;
 using Entities.Player.Scripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using Slider = UnityEngine.UIElements.Slider;
 
 namespace General.Scripts
 {
@@ -20,9 +18,9 @@ namespace General.Scripts
         public GameObject playerPrefab;
 
         public static GameController Instance;
-        [SerializeField] private Canvas canvasPausa;
-        [SerializeField] private Canvas canvasGameOver;
-        [SerializeField] private Canvas canvasOptions;
+        [SerializeField] private GameObject menuPausa;
+        [SerializeField] private GameObject menuGameOver;
+        [SerializeField] private GameObject menuOptions;
         [SerializeField] private PlayerData playerData;
         private GameObject _jugador;
         private bool _useCheckpoint;
@@ -55,9 +53,9 @@ namespace General.Scripts
             {
                 Instance = this;
                 DontDestroyOnLoad(transform.gameObject);
-                DontDestroyOnLoad(canvasPausa);
-                DontDestroyOnLoad(canvasGameOver);
-                DontDestroyOnLoad(canvasOptions);
+                //DontDestroyOnLoad(canvasPausa);
+                //DontDestroyOnLoad(canvasGameOver);
+                //DontDestroyOnLoad(canvasOptions);
                 gameData = SaveLoadManager.LoadGame(PlayerPrefs.GetString("slot"));
                 if (gameData.isValid)
                 {
@@ -75,9 +73,9 @@ namespace General.Scripts
             }
             else
             {
-                Destroy(canvasPausa.gameObject);
-                Destroy(canvasGameOver.gameObject);
-                Destroy(canvasOptions.gameObject);
+                //Destroy(canvasPausa.gameObject);
+                //Destroy(canvasGameOver.gameObject);
+                //Destroy(canvasOptions.gameObject);
                 Destroy(gameObject);
                 Destroy(_jugador);
             }
@@ -131,7 +129,7 @@ namespace General.Scripts
 
         public void SceneLoad(SPlayerSpawnData spawnData, bool useCheckpoint)
         {
-            canvasGameOver.gameObject.SetActive(false);
+            menuGameOver.SetActive(false);
             _useCheckpoint = useCheckpoint;
             _playerSpawnDataInNewScene = spawnData;
             if (spawnData.Scene.GetSceneName() == null || spawnData.Scene.GetSceneName().Equals("") )
@@ -153,14 +151,14 @@ namespace General.Scripts
         public void Pause()
         {
             Time.timeScale = 0;
-            canvasPausa.gameObject.SetActive(true);
+            menuPausa.SetActive(true);
         }
     
         public void PauseContinue()
         {
             Time.timeScale = 1;
             StartCoroutine(Wait());
-            canvasPausa.gameObject.SetActive(false);
+            menuPausa.SetActive(false);
             _jugador.GetComponent<PlayerController>().EnablePlayerControls();
         }
         
@@ -176,9 +174,9 @@ namespace General.Scripts
         }
         public void DeletePersistentElement()
         {
-            Destroy(canvasPausa.gameObject);
-            Destroy(canvasGameOver.gameObject);
-            Destroy(canvasOptions.gameObject);
+            //Destroy(canvasPausa.gameObject);
+            //Destroy(canvasGameOver.gameObject);
+            //Destroy(canvasOptions.gameObject);
             Destroy(gameObject);
             Time.timeScale = 1;
         }
@@ -191,22 +189,27 @@ namespace General.Scripts
         public void GameOver()
         {
             Time.timeScale = 0;
-            canvasGameOver.gameObject.SetActive(true);
+            _jugador.GetComponent<PlayerController>().DisablePlayerControls();
+            menuGameOver.SetActive(true);
         }
         
         public void showOptions()
         {
-            Time.timeScale = 0;
-            canvasPausa.gameObject.SetActive(false);
-            canvasOptions.gameObject.SetActive(true);
-            //canvasOptions.
+            _jugador.GetComponent<PlayerController>().DisablePlayerControls();
+            menuPausa.SetActive(false);
+            menuOptions.SetActive(true);
         }
         
         public void hideOptions()
         {
-            canvasOptions.gameObject.SetActive(false);
-            canvasPausa.gameObject.SetActive(true);
+            menuPausa.SetActive(true);
+            menuOptions.SetActive(false);
+           
         }
-        
+        public void CallSceneLoad()
+        {
+            GameController.Instance.SceneLoad(GameController.Instance.GetCheckpoint(),true);
+            //gameControler.GetComponent<GameController>().SceneLoad(gameControler.GetComponent<GameController>().GetCheckpoint());
+        }
     }
 }
