9b555b50b816b33f384654ff918a98ca3fb237f5
Markel <markel.fernandezdelabastida@ikasle.egibide.org>
2024-02-15 17:45:53 +0100

    [Markel] Base potion audios

--- UnityPrototypeEgibide2023/Assets/Entities/Potions/BasePotion/Scripts/ProjectileComponent.cs
+++ UnityPrototypeEgibide2023/Assets/Entities/Potions/BasePotion/Scripts/ProjectileComponent.cs
@@ -5,16 +5,17 @@ namespace Entities.Potions.BasePotion.Scripts
     public class ProjectileComponent : MonoBehaviour
     {
         private AttackComponent[] _attackComponents;
-        
+        private AudioSource _audioSource;
         public float delay = 0f;
         public float damageActiveTime = 0.5f;
         public float destroyTime = 1f;
         public int speed = 0;
+        public Audios _audios;
         void Awake()
         {
             _attackComponents = GetComponentsInChildren<AttackComponent>(true);
             Invoke(nameof(ActivateHitbox), delay);
-
+            _audioSource = GetComponent<AudioSource>();
             Despawn();
         }
         
@@ -33,7 +34,10 @@ namespace Entities.Potions.BasePotion.Scripts
         
         private void Destroy()
         {
+            _audioSource.clip = _audios.audios[0];
+            _audioSource.Play();
             Destroy(gameObject);
+            
         }
 
     }
