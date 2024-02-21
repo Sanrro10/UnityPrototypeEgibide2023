4c43f5fb55074512399d67b621f7a96a96c5660d
Gbertol <gbertol@egibide.org>
2024-02-21 17:01:02 +0100

    Mas repaso ,iluminacion cuevas,sonido,particulas checkpoint

--- UnityPrototypeEgibide2023/Assets/General/Scripts/CheckpointManager.cs
+++ UnityPrototypeEgibide2023/Assets/General/Scripts/CheckpointManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Entities.Player.Scripts;
@@ -7,26 +8,43 @@ using UnityEngine;
 public class CheckpointManager : MonoBehaviour
 {
     private Vector3 _checkpoint;
-    
+    [SerializeField] ParticleSystem particles;
+    private AudioSource audioFX;
+    [SerializeField] float coolDownT;
+    private bool enfriado = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        _checkpoint = transform.position; 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        _checkpoint = transform.position;
+        audioFX = GetComponentInChildren<AudioSource>();
         
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Colision con el player
         if (collision.CompareTag("Player"))
         {
+             
             GameController.Instance.GetComponent<GameController>().SetCheckpoint(_checkpoint);
             collision.gameObject.GetComponent<PlayerController>().heal();
+            if (enfriado)
+            {
+                particles.Play();
+                audioFX.Play();
+                enfriado = false;
+
+                StartCoroutine(CoolDown());
+            }
         }
+    }
+
+    private IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(coolDownT);
+        enfriado = true;
+
     }
 }
