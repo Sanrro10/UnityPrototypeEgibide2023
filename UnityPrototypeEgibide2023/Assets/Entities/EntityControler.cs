c93afef5c3a98edf4e19d1dfa87c2699e0a3b3e7
AlejandroMacazaga <alejandrolopezmacazaga@gmail.com>
2024-02-23 16:50:46 +0100

    ole

--- UnityPrototypeEgibide2023/Assets/Entities/EntityControler.cs
+++ UnityPrototypeEgibide2023/Assets/Entities/EntityControler.cs
@@ -66,5 +66,6 @@ namespace Entities
         {
             return Rb;
         }
+        
     }
 }
---
+++ UnityPrototypeEgibide2023/Assets/Entities/Zarzas.cs
@@ -0,0 +1,19 @@
+﻿namespace Entities
+{
+    public class Zarzas : EntityControler
+    {
+        public void Start()
+        {
+           Health.Set(1);
+        }
+        
+        public override void OnReceiveDamage(AttackComponent.AttackData attack, bool toTheRight = true)
+        {
+            // nothing 
+        }
+        public override void OnDeath()
+        {
+            
+        }
+    }
+}
